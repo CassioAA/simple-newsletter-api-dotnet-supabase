c8ab27d20e905bba73880c5fdb01823cad566beb
CassioAA <cassioalmeidaccti@gmail.com>
2025-02-07 09:31:34 -0300

    add: Model and contracts

---
+++ Contracts/CreateNewsletterRequest.cs
@@ -0,0 +1,10 @@
+namespace NewsletterAPI.Contracts;
+
+public class CreateNewsletterRequest
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public int ReadTime { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Supabase;
+using Supabase.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<Client>(_ =>
+    new Client(
+        builder.Configuration["SupabaseUrl"],
+        builder.Configuration["SupabaseKey"],
+        new SupabaseOptions
+        {
+            AutoRefreshToken = true,
+            AutoConnectRealtime = true
+        }
+    )
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
